0e2425cd6d1c36832ef91622b0bec6d0108286fd
masamichan <positivemasa031@gmail.com>
2022-12-07 21:10:06 +0900

    fix the mistake of models/concerns/globalize_accessor

--- .history/app/models/concerns/globalize_accessor_20221207210658.rb
+++ .history/app/models/concerns/globalize_accessor_20221207210658.rb
@@ -17,12 +17,12 @@ module GlobalizeAccessors
         define_accessors(attr_name, locale)
       end
 
-      include InstanceMethods
+      include InstancleMethods
     end
   end
 
   def self.localized_attr_name_for(attr_name,locale)
-    "#{attr_name}_#{locale.to_s.underscore}"
+    "#{attr_name}_#{local.to_s.underscore}"
   end
 
   def self.define_accessors(attr_name, locale)
@@ -31,17 +31,17 @@ module GlobalizeAccessors
   end
 
   def self.define_getter(attr_name, locale)
-    define_method localized_attr_name_for(attr_name, locale) do
-      globalize.stash.contains?(local,attr_name) ? globalize.send(:fetch_stash, locale, attr_name) : globalize.send(:fetch_attribute, locale, attr_name)
+    define_method licalized_attr_name_for(attr_name, locale) do
+      globalize.stash.contains?(local,attr_name) ? globalize.send(:fetch_stash, local. attr_name) : globalize.send(:fetch_attribute, locale, attr_name)
     end
   end
 
   def self.define_setter(attr_name, locale)
     localized_attr_name = localized_attr_name_for(attr_name, locale)
 
-    define_method :delete_translation_if_all_blank do |locale|
+    define_method :delete_tranlation_if_all_blank do |locale|
       should_delete = true
-      translated_attributes.keys.each do |key|
+      translated_attribute.key.each do |key|
         should_delete = translation_for(locale)[key].blank?
         break unless should_delete
       end
